d44d6856f9abe75349b03ae89cc4440fa299af4b
wirix <88577048+wirix@users.noreply.github.com>
2023-04-03 23:06:51 +0700

    'анимация-выпадения-меню-на-телефоне'

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
 import styles from './Header.module.css';
 import { HeaderProps } from './Header.props';
+import { ButtonIcon } from '../../components';
+import { motion, useReducedMotion } from 'framer-motion';
+import { Sidebar } from '../Sidebar/Sidebar';
+import { useRouter } from 'next/router';
 
-export const Header = ({ ...props }: HeaderProps): JSX.Element => {
+export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
+  const [isOpened, setIsOpened] = useState(false);
+  const shouldReducedMotion = useReducedMotion();
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsOpened(false);
+  }, [router]);
+
+  const variants = {
+    opened: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        // вид анимации
+        stiffness: 20
+      }
+    },
+    closed: {
+      opacity: shouldReducedMotion ? 1 : 0,
+      x: '100%',
+    }
+  };
+
   return (
-    <div {...props}>
-      Header
-    </div>
+    <header className={cn(className, styles.header)} {...props}>
+      LOGO
+      <ButtonIcon appearance='white' icon='menu' onClick={() => { setIsOpened(true); }} />
+      <motion.div
+        className={styles.mobileMenu}
+        variants={variants}
+        initial={'closed'}
+        animate={isOpened ? 'opened' : 'closed'}
+      >
+        <Sidebar />
+        <ButtonIcon className={styles.menuClose} appearance='white' icon='close' onClick={() => { setIsOpened(false); }} />
+      </motion.div>
+    </header>
   );
 };
